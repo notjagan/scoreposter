482dcad144f51d2cc65edecf259d1df52658c280
Jagan Prem <notjagan@gmail.com>
2021-02-20 21:54:19 -0500

    Fix indentation

--- scoreposter/title.py
+++ scoreposter/title.py
@@ -16,54 +16,54 @@ class TitleOptions:
 
 
 def construct_title(score, options):
-        if score.mods:
-            modstring = ''.join(string for mod, string in MODS.items()
-                                if mod in score.mods)
-            base = f"{score.artist} - {score.title} [{score.difficulty}] +{modstring} ({score.stars:.2f}*)"
+    if score.mods:
+        modstring = ''.join(string for mod, string in MODS.items()
+                            if mod in score.mods)
+        base = f"{score.artist} - {score.title} [{score.difficulty}] +{modstring} ({score.stars:.2f}*)"
+    else:
+        base = f"{score.artist} - {score.title} [{score.difficulty}] ({score.stars:.2f}*)"
+
+    fc = score.misses == 0 and options.sliderbreaks == 0
+
+    if score.accuracy == 100:
+        base += " SS"
+    else:
+        base += f" {score.accuracy:.2f}%"
+        if score.misses != 0:
+            base += f" {score.misses}xMiss"
+        if options.sliderbreaks != 0:
+            base += f" {options.sliderbreaks}xSB"
+        if options.show_combo or not fc:
+            base += f" {score.combo}/{score.max_combo}x"
+        if fc:
+            base += " FC"
+
+    if score.ranking is not None:
+        base += f" #{score.ranking}"
+    if score.loved:
+        base += " LOVED"
+
+    segments = [score.player, base]
+
+    if options.show_pp:
+        pp_text = f"{score.pp:.0f}pp"
+        if not score.ranked:
+            pp_text += " if ranked"
+        elif not score.submitted:
+            pp_text += " if submitted"
+        if options.show_fc_pp and not fc:
+            pp_text += f" ({score.fcpp:.0f}pp for FC)"
+        segments.append(pp_text)
+
+    if options.show_ur and score.ur is not None:
+        dt = Mod.DoubleTime in score.mods or Mod.Nightcore in score.mods
+        if dt:
+            segments.append(f"{score.ur:.2f} cv.UR")
         else:
-            base = f"{score.artist} - {score.title} [{score.difficulty}] ({score.stars:.2f}*)"
+            segments.append(f"{score.ur:.2f} UR")
 
-        fc = score.misses == 0 and options.sliderbreaks == 0
+    if options.message is not None:
+        segments.append(options.message)
 
-        if score.accuracy == 100:
-            base += " SS"
-        else:
-            base += f" {score.accuracy:.2f}%"
-            if score.misses != 0:
-                base += f" {score.misses}xMiss"
-            if options.sliderbreaks != 0:
-                base += f" {options.sliderbreaks}xSB"
-            if options.show_combo or not fc:
-                base += f" {score.combo}/{score.max_combo}x"
-            if fc:
-                base += " FC"
-
-        if score.ranking is not None:
-            base += f" #{score.ranking}"
-        if score.loved:
-            base += " LOVED"
-
-        segments = [score.player, base]
-
-        if options.show_pp:
-            pp_text = f"{score.pp:.0f}pp"
-            if not score.ranked:
-                pp_text += " if ranked"
-            elif not score.submitted:
-                pp_text += " if submitted"
-            if options.show_fc_pp and not fc:
-                pp_text += f" ({score.fcpp:.0f}pp for FC)"
-            segments.append(pp_text)
-
-        if options.show_ur and score.ur is not None:
-            dt = Mod.DoubleTime in score.mods or Mod.Nightcore in score.mods
-            if dt:
-                segments.append(f"{score.ur:.2f} cv.UR")
-            else:
-                segments.append(f"{score.ur:.2f} UR")
-
-        if options.message is not None:
-            segments.append(options.message)
-
-        title = ' | '.join(segments)
-        return title
+    title = ' | '.join(segments)
+    return title
